7a08ae08304c47bc10ba9e1ff37eef89beeba900
Alfred Vernet <mail@alfredvernet.cat>
2023-12-27 18:26:29 +0100

    Update member.js

--- member.js
+++ member.js
@@ -1,1 +1,15 @@
-function skillsMember(
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsBtn = document.getElementById("memberSkillsBtn");
+    var memberSkillsBtnIcon = document.getElementById("memberSkillsBtnIcon");
+    if (memberSkills.style.display === "none") {
+        memberSkills.style.display = "block";
+        memberSkillsBtnIcon.style.transform = "rotate(180deg)";
+        member.style.height = "auto";
+    } else {
+        memberSkills.style.display = "none";
+        memberSkillsBtnIcon.style.transform = "rotate(0deg)";
+        member.style.height = "auto";
+    }
+}
